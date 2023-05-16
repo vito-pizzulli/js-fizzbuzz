83f04bc0fd22c3b381b3718d42a8d4f4fed6d431
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-05-16 16:20:54 +0200

    bonus 2, add style according to line content, done

--- js/myscript.js
+++ js/myscript.js
@@ -1,18 +1,27 @@
 const numberList = document.querySelector('div.container');
 let element;
+let output;
 
 for (let i = 1; i <= 100; i++) {
+    output = document.createElement('p');
+    output.classList.add('goldenrod');
+
     if ((i % 3 === 0) && (i % 5 === 0)) {
         element = "FizzBuzz";
+        output.classList = " ";
+        output.classList.add('red');
     } else if (i % 3 === 0) {
         element = "Fizz";
+        output.classList = " ";
+        output.classList.add('blue');
     } else if (i % 5 === 0) {
         element = "Buzz";
+        output.classList = " ";
+        output.classList.add('green');
     } else {
         element = i;
     }
 
-    output = document.createElement('p');
-    output.append("Riga " + i + " - " + element);
+    output.append("Line " + i + " - " + element);
     numberList.append(output);
 }
