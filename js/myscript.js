da0679c958a02d1d63869b741b3380daceb5d248
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-05-16 16:46:26 +0200

    better styling with responsive layouts done

--- js/myscript.js
+++ js/myscript.js
@@ -4,20 +4,20 @@ let output;
 
 for (let i = 1; i <= 100; i++) {
     output = document.createElement('p');
-    output.classList.add('goldenrod');
+    output.classList.add('bgNumber');
 
     if ((i % 3 === 0) && (i % 5 === 0)) {
         element = "FizzBuzz";
         output.classList = " ";
-        output.classList.add('red');
+        output.classList.add('bgFizzBuzz');
     } else if (i % 3 === 0) {
         element = "Fizz";
         output.classList = " ";
-        output.classList.add('blue');
+        output.classList.add('bgFizz');
     } else if (i % 5 === 0) {
         element = "Buzz";
         output.classList = " ";
-        output.classList.add('green');
+        output.classList.add('bgBuzz');
     } else {
         element = i;
     }
