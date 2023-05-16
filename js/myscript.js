9cafa391d926535ff23cb761cb6189bf50acf265
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-05-16 15:56:57 +0200

    created working fizzbuzz for cycle, exer completed

--- js/myscript.js
+++ js/myscript.js
@@ -1,8 +1,18 @@
 const numberList = document.querySelector('main');
+let element;
 
 for (let i = 1; i <= 100; i++) {
-    console.log(i);
-    const number = document.createElement('p');
-    number.append(i);
-    numberList.append(number);
+    if ((i % 3 === 0) && (i % 5 === 0)) {
+        element = "FizzBuzz";
+    } else if (i % 3 === 0) {
+        element = "Fizz";
+    } else if (i % 5 === 0) {
+        element = "Buzz";
+    } else {
+        element = i;
+    }
+
+    output = document.createElement('p');
+    output.append(element);
+    numberList.append(output);
 }
